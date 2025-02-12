2bd6632d4d2923dc69c304b360b46515c14f924e
DucK215480106013 <k215480106013@tnut.edu.vn>
2025-02-12 20:34:04 +0700

    hoan thanh

--- app/src/main/java/com/example/bai1/TinhToan.java
+++ app/src/main/java/com/example/bai1/TinhToan.java
@@ -1,11 +1,14 @@
 package com.example.bai1;
 
+import android.webkit.JavascriptInterface;
+
 public class TinhToan {
     private double r;
-
+    @JavascriptInterface
     public Double tinhChuVi(){
         return r*2*Math.PI;
     }
+    @JavascriptInterface
     public Double tinhDientich(){
         return Math.pow(r,2)*Math.PI;
     }
